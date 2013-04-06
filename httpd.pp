de8e5a452c1681da8233e5679c2164bb59e29354
potionz <potion.potion@gmail.com>
2013-04-06 16:47:27 +0200

    load module mod_cgi.so and service requires added

--- httpd.pp
+++ httpd.pp
@@ -6,10 +6,18 @@ package { $httpd:
 	ensure => installed,
 	alias => apache
 }
+file { '/etc/httpd/conf/cgi.conf':
+	path => '/etc/httpd/conf/',
+	ensure => present,
+	contetn => "LoadModule cgi_module modules/mod_cgi.so",
+}
+
 
 service { $httpd:
 	enable => true,
-	ensure => running
+	ensure => running,
+	hasrestart => true,
+	require => Package['httpd'],	
 }
 
 
