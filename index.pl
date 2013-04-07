9042a4365f489f6a081ffb101aa5d2b3234de399
potionz <potion.potion@gmail.com>
2013-04-07 13:25:48 +0200

    index.pl updated

--- index.pl
+++ index.pl
@@ -1,17 +1,17 @@
 #!/usr/bin/perl
+print "Content-type: text/html\n";
 
-print "Content-type: text/html\n\n";
-
-print "<HTML>";
-print "<HEAD>";
-print "<TITLE>Hello World</TITLE>";
-print "</HEAD>";
-print "<BODY>";
-print "<H1>Fedora</H1>";
-print "</BODY>";
-print "</HTML>";
+print <<"EOF123";
+<HTML>
+<head>
+<title>
+Perl
+</title>
+</head>
+EOF123
 
 print `uptime`;
+
 
 exit;
  
