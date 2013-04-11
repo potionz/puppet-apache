87c0118e52d0c4a6891e31720e74f592b3ef78da
potionz <potion.potion@gmail.com>
2013-04-11 13:12:13 +0200

    httpd.pp comments of puppet recipe was added

--- httpd.pp
+++ httpd.pp
@@ -1,28 +1,25 @@
-package { 'httpd':
-	ensure => installed,
-	alias => apache,
+package { 'httpd':			# name of package which
+	ensure => installed,		# ensure package is installed, if not puppet will install this package
+	alias => apache,		
 
 }
-file { '/etc/httpd/conf/cgi.conf':
-	notify => Service['httpd'],
-	mode => 644,
+file { '/etc/httpd/conf/cgi.conf':	# set path and file name for cgi config
+	notify => Service['httpd'],	# this will notify service httpd if config will be changed
+	mode => 644,			# set permisions for cofing file
 	owner => root,
-	group => root,
-	content => template('/home/base/puppet/template/cgi.conf'),	
-	require => Package['httpd'],
+	group => root,			# set owner and group of config file 
+	content => template('/home/base/puppet/template/cgi.conf'),	# this is main cgi config file, all changes for cgi configuration must be done here in template and it also take effect in /etc/httpd/conf/cgi.conf!
+	require => Package['httpd'],	# dependencies on installed package httpd
 }
-file { '/var/www/cgi-bin/index.pl':
-	mode => 755,
-	owner => apache,
-	group => apache,
-	content => template('/home/base/puppet/scripts/index.pl'),
+file { '/var/www/cgi-bin/index.pl':	# destination of cgi index file
+	mode => 755,			# permisions of file
+	owner => apache,		# owner of file
+	group => apache,		# group of file
+	content => template('/home/base/puppet/scripts/index.pl'),	# source file of cgi script, all changes for index.pl file must be done here in template and it also take effect on /var/www/cgi-bin/index.pl!
 }	
-service { 'httpd':
-	enable => true,
-	ensure => running,
-	hasrestart => true,
-	require => Package['httpd'],
+service { 'httpd':			# select service httpd
+	enable => true,			# enable service
+	ensure => running,		# test if service is running
+	hasrestart => true,		# set up that service will be restarted with init restart, not with stop /start script
+	require => Package['httpd'],	# set up dependencies on package httpd
 }
-
-
-
