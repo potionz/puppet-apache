aecccdb54967b05bb57f9c0f1b55b2b018d503c7
potionz <potion.potion@gmail.com>
2013-04-15 11:19:42 +0200

    perl.pp recipe was rewrite to shorter version

--- perl.pp
+++ perl.pp
@@ -1,8 +1,3 @@
-package {'perl':			# check if perl package is installed
-	ensure => installed,		# and if not then it will install it
-}
 package {'perl-CGI':			# check if perl-CGI is installed
-	ensure => installed,		# 
-	require => Package['perl'],	# and set dependencies that perl package is required
+	ensure => installed,		# if not it will install perl and perl-CGI
 }
-
